96707a1d1f17eb850157a984c1a9f13413bce212
ledoctah <matheusferr2015@gmail.com>
2021-01-03 13:27:27 -0300

    Added global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-const App: React.FC = () => <h1>Hello, World!</h1>
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => (
+  <>
+    <h1>Hello, World!</h1>
+    <GlobalStyle />
+  </>
+);
 
 export default App;
