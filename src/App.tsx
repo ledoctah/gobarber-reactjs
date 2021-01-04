42724bf0c25e7c3fbdc9969078a69f6acf0bd455
ledoctah <matheusferr2015@gmail.com>
2021-01-04 16:04:03 -0300

    Styling ToastMessages

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,15 @@ import GlobalStyle from './styles/global';
 import SignIn from './pages/SignIn';
 
 import { AuthProvider } from './hooks/AuthContext';
+import ToastContainer from './components/ToastContainer';
 
 const App: React.FC = () => (
   <>
     <AuthProvider>
       <SignIn />
     </AuthProvider>
+
+    <ToastContainer />
 
     <GlobalStyle />
   </>
